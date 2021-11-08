899a96e754b387b872bb0f3edfc6c1023d340278
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-11-08 12:40:36 +0100

    feat: webmanifest and favicon added

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <meta charset="UTF-8">
+    <link rel="icon" href="./favicon.ico">
+    <link rel="manifest" href="./webmanifest.json">
     <title>URL Expander</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 </head>
